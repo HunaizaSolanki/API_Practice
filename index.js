76be0dbf103d0a72cecb0b258a437f7b8cdec766
HunaizaSolanki <hunaizariaz10@gmail.com>
2023-06-01 17:33:40 +0500

    01/06

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // create server
 const http = require("http");
 const app = require("./app");
+const port = process.env.PORT || 3000
 const server = http.createServer(app);
 
-server.listen(3000, console.log("App is running"));
+server.listen(port, console.log("App is running"));
